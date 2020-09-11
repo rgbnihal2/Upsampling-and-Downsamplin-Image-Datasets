e3b83c84beaac3922e95018f6a4d9e85820128cf
Ragib Nihal <31960449+rgbnihal2@users.noreply.github.com>
2020-09-11 09:20:47 +0600

    Add files via upload

---
+++ undesampling.m
@@ -0,0 +1,29 @@
+clc;
+clear;
+imagepath = fullfile('Test');
+imdsTrain = imageDatastore(imagepath,'IncludeSubfolders',true,'LabelSource','Foldernames');
+
+labelCount = countEachLabel(imdsTrain);histogram(imdsTrain.Labels);title('Class frequency');
+labels=imdsTrain.Labels;
+[G,classes] = findgroups(labels);
+numObservations = splitapply(@numel,labels,G);
+desiredNumObservationsPerClass = min(numObservations);
+files = splitapply(@(x){randReplicateFiles(x,desiredNumObservationsPerClass)},imdsTrain.Files,G);
+files = vertcat(files{:});
+
+%%
+labels=[];info=strfind(files,'\');
+for i=1:numel(files)
+    idx=info{i};
+    dirName=files{i};
+    targetStr=dirName(idx(end-1)+1:idx(end)-1);
+    targetStr2=cellstr(targetStr);
+    labels=[labels;categorical(targetStr2)];
+end
+imdsTrain.Files = files;
+imdsTrain.Labels=labels;
+labelCount_oversampled = countEachLabel(imdsTrain)
+histogram(imdsTrain.Labels)
+
+title('Class frequency');
+%%
